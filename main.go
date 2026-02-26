38104aad249a9f585b8f85e7ccd68753be4b3e66
Kevin Thiels <kevin.thiels@aperta.be>
2026-02-26 12:50:51 +0100

    feat: wire router, middleware, and handlers in main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+	"github.com/Novitsh/dummy-training-app/config"
+	"github.com/Novitsh/dummy-training-app/handlers"
+	"github.com/Novitsh/dummy-training-app/middleware"
+)
+
+func createRenderer() multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+	r.AddFromFiles("home.html", "templates/layout.html", "templates/home.html")
+	r.AddFromFiles("about.html", "templates/layout.html", "templates/about.html")
+	return r
+}
+
+func main() {
+	cfg := config.Load()
+
+	r := gin.New()
+	r.Use(middleware.Logger())
+	r.Use(gin.Recovery())
+
+	r.HTMLRender = createRenderer()
+	r.Static("/static", "./static")
+
+	r.GET("/", handlers.Home)
+	r.GET("/about", handlers.About)
+
+	addr := fmt.Sprintf(":%s", cfg.Port)
+	r.Run(addr)
+}
